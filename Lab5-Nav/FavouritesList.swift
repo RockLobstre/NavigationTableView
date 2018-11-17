af90ffcbaaeb473b0b930b1565ff569013ea1906
matin <hsalehi91@gmail.com>
2018-11-17 16:21:17 -0500

    initial Commit

---
+++ Lab5-Nav/FavouritesList.swift
@@ -0,0 +1,14 @@
+//
+//  FavouriteList.swift
+//  Lab5-Nav
+//
+//  Created by Matin Salehi on 2018-11-17.
+//  Copyright © 2018 Matin Salehi. All rights reserved.
+//
+
+import UIKit
+
+class FavouritesList {
+
+
+}
---
+++ Lab5-Nav/RootViewController.swift
@@ -0,0 +1,36 @@
+//
+//  RootViewController.swift
+//  Lab5-Nav
+//
+//  Created by Matin Salehi on 2018-11-17.
+//  Copyright © 2018 Matin Salehi. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITableViewController {
+
+    private var familyNames: [String]!
+    private var cellPointSize: CGFloat!
+    private var favouriteList: FavouritesList!
+    private static let familyCell = "FamilyName"
+    private static let favouritesCell = "Favourites"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
